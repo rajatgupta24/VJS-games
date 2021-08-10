7b467f7f329a907c8c0e34609aaf65297632d900
rajat <rajat2411gupta@gmail.com>
2021-08-10 16:27:26 +0530

    Mobile view of snake game complete

--- games/tetris/game.js
+++ games/tetris/game.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
   const scoreDisplay = document.querySelector('#score')
-  const startBtn = document.querySelector('#start-button')
+  const startBtn = document.querySelector('#startBtn')
   const width = 10
   let nextRandom = 0
   let timerId
@@ -27,8 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
     'green',
     'blue'
   ]
-
-  //The Tetrominoes
   const lTetromino = [
     [1, width+1, width*2+1, 2],
     [width, width+1, width+2, width*2+2],
@@ -69,13 +67,9 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentPosition = 4
   let currentRotation = 0
 
-  console.log(theTetrominoes[0][0])
-
-  //randomly select a Tetromino and its first rotation
   let random = Math.floor(Math.random()*theTetrominoes.length)
   let current = theTetrominoes[random][currentRotation]
 
-  //draw the Tetromino
   function draw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
@@ -83,7 +77,6 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  //undraw the Tetromino
   function undraw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.remove('tetromino')
